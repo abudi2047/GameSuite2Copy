8348becdf38824576a5adffe870230e29b8e6746
SajidA <abudi.sajid@vodafone.com>
2023-08-23 10:56:32 +0100

    Added comments to FindThePairActivity.kt

--- app/src/main/java/com/example/gamesuite2/FindThePairActivity.kt
+++ app/src/main/java/com/example/gamesuite2/FindThePairActivity.kt
@@ -3,7 +3,7 @@ package com.example.gamesuite2
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.widget.GridView
-
+//added comment
 class FindThePairActivity : AppCompatActivity() {
 
     private lateinit var adapter: PairAdapter
